b5c3e591200e007e3093e98f30dd17e5b0c48761
Johan Euphrosine <proppy@google.com>
2017-01-31 12:08:30 -0800

    sample-nativepio: initial import
    Change-Id: I945b73b1b76e29114eb6503e5172154f7f7f5d80

---
+++ README.md
@@ -0,0 +1,85 @@
+Simple example of Android Things Native Peripheral I/O APIs
+===========================================================
+
+This Android Things app runs basic code that exercises the Native PIO APIs from
+C++. Each sample is an Android module that can be run independently.
+
+**IMPORTANT**: Please, note that these samples are not necessarily the easiest way to accomplish
+a task. In particular, they handle all low level I/O protocols directly, on
+purpose to showcase how to use the Peripheral APIs. In real world applications,
+you should use or develop a suitable driver that encapsulates the manipulation
+of low level APIs.
+
+Pre-requisites
+--------------
+
+- Android Things compatible board
+- Android Studio 2.2+
+
+For the Blink sample:
+- 1 LED
+- 1 resistor
+- 2 jumper wires
+- 1 breadboard
+
+Build and install
+=================
+
+[Download](https://developer.android.com/things/preview/download.html) the Android Things native library and extract it in the project root directory.
+
+It should contains the following directories:
+```
+libandroidthings/
+  ${ABI}/
+    include/
+      pio/
+        *.h
+    lib/
+      libandroidthings.so
+```
+
+On Android Studio, select the module in the select box by the "Run" button, and
+then click on the "Run" button.
+
+If you prefer to run on the command line, type
+
+```bash
+./gradlew <module>:installDebug
+adb shell am start com.example.androidthings.simplepio/.<ModuleActivity>
+```
+
+Sample Specifics
+================
+
+Blink
+-----
+
+![Schematics for Intel Edison](blink/edison_schematics.png)
+![Schematics for Raspberry Pi 3](blink/rpi3_schematics.png)
+
+```bash
+    ./gradlew blink:installDebug
+    adb shell am start com.example.androidthings.simplepio/.BlinkActivity
+```
+
+Blinks an LED connected to a GPIO pin.
+
+License
+=======
+
+Copyright 2016 The Android Open Source Project, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ blink/src/main/cpp/blink.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <cmath>
+
+#include <android/log.h>
+#include <android_native_app_glue.h>
+
+#include <pio/gpio.h>
+#include <pio/peripheral_manager_client.h>
+
+#include "AndroidSystemProperties.h"
+
+const int BLINK_INTERVAL_MS = 1000;
+const char* TAG = "blink";
+
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define ASSERT(cond, ...) if (!(cond)) { __android_log_assert(#cond, TAG, __VA_ARGS__);}
+
+void android_main(android_app* app) {
+    app_dummy(); // prevent native-app-glue to be stripped.
+
+    AndroidSystemProperties systemProperties(app->activity);
+    const char* LED_GPIO;
+    if (systemProperties.getBuildDevice() == "rpi3") {
+        LED_GPIO = "BCM6";
+    } else {
+        LOGE("unsupported device: %s", systemProperties.getBuildDevice().c_str());
+        return;
+    }
+
+    APeripheralManagerClient* client = APeripheralManagerClient_new();
+    ASSERT(client, "failed to open peripheral manager client");
+    AGpio* gpio;
+    APeripheralManagerClient_openGpio(client, LED_GPIO, &gpio);
+    ASSERT(gpio, "failed to open GPIO: %s", LED_GPIO);
+    int result = AGpio_setDirection(gpio, AGPIO_DIRECTION_OUT_INITIALLY_LOW);
+    ASSERT(result == 0, "failed to set direction for GPIO: %s", LED_GPIO);
+
+    while (!app->destroyRequested) {
+        int gpioValue;
+        if (AGpio_getValue(gpio, &gpioValue) < 0) {
+            LOGE("failed to get value for GPIO: %s", LED_GPIO);
+            continue; // retry immediately
+        }
+        if (AGpio_setValue(gpio, !gpioValue) < 0) {
+            LOGE("failed to set value for GPIO: %s", LED_GPIO);
+            continue; // retry immediately
+        }
+        android_poll_source* source;
+        // wait for at most BLINK_INTERVAL_MS.
+        // proper control loop implementation would also account for the time spent handling
+        // lifecycle and input events.
+        int pollResult = ALooper_pollOnce(BLINK_INTERVAL_MS, NULL, NULL, (void**)&source);
+        if (pollResult >= 0) {
+            if (source != NULL) {
+                // forward event to native-app-glue to handle lifecycle and input event
+                // and update `app` state.
+                source->process(app, source);
+            }
+        }
+    }
+    AGpio_delete(gpio);
+    APeripheralManagerClient_delete(client);
+}
